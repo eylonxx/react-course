d033baa879aa83c81c6a6e06ac35763267e0cf72
Eylon <eylon.perets@gmail.com>
2022-04-17 14:43:46 +0300

    card exercise

--- 17_18_lifecycle/src/Card.js
+++ 17_18_lifecycle/src/Card.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import './Card.css';
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    let angle = Math.random() * 90 - 45;
+    let xPos = Math.random() * 40 - 20;
+    let yPos = Math.random() * 40 - 20;
+
+    this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+  }
+
   render() {
     console.log(this.props.imgSrc);
-    return <img className="Card" src={this.props.imgSrc} alt={'banana'} />;
+    return <img style={{ transform: this._transform }} className="Card" src={this.props.imgSrc} alt={'banana'} />;
   }
 }
