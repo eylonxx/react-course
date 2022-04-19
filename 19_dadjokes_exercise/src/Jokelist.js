3dc59735c55f69b2b6fe4d7f4007dd762648b7c8
Eylon <eylon.perets@gmail.com>
2022-04-19 10:23:59 +0300

    added duplicate handling

--- 19_dadjokes_exercise/src/Jokelist.js
+++ 19_dadjokes_exercise/src/Jokelist.js
@@ -8,28 +8,57 @@ export default class Jokelist extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: [],
+      jokes: JSON.parse(localStorage.getItem('jokes') || '[]'),
+      loading: false,
     };
+    this.seenJokes = new Set(this.state.jokes.map((j) => j.text));
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    if (this.state.jokes.length === 0) {
+      this.getJokes();
+    }
+  }
+
+  async getJokes() {
     const jokes = [];
     while (jokes.length < 10) {
       let res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
       console.log('hi');
-      jokes.push({ text: res.data.joke, votes: 0, id: uuidv4() });
+      if (!this.seenJokes.has(res.data.joke)) {
+        jokes.push({ text: res.data.joke, votes: 0, id: uuidv4() });
+      }
     }
-    this.setState({ jokes: jokes });
-  }
-  componentWillUnmount() {
-    console.log('aaaaaaas');
+    this.setState(
+      (st) => ({
+        loading: false,
+        jokes: [...st.jokes, ...jokes],
+      }),
+      () => localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
   }
 
   handleVote(id, delta) {
-    this.setState((st) => ({ jokes: st.jokes.map((j) => (j.id === id ? { ...j, votes: j.votes + delta } : j)) }));
+    this.setState(
+      (st) => ({ jokes: st.jokes.map((j) => (j.id === id ? { ...j, votes: j.votes + delta } : j)) }),
+      () => localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
+  }
+
+  handleClick() {
+    this.setState({ loading: true }, this.getJokes);
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div className="Jokelist-spinner">
+          <i className="far fa-8x fa-laugh fa-spin"></i>
+          <h1 className="Jokelist-title">Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className="Jokelist">
         <div className="Jokelist-sidebar">
@@ -40,7 +69,9 @@ export default class Jokelist extends Component {
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt="lol"
           />
-          <button className="Jokelist-getMore">New Jokes</button>
+          <button onClick={this.handleClick} className="Jokelist-getMore">
+            New Jokes
+          </button>
         </div>
         <div className="Jokelist-jokes">
           {this.state.jokes.map((joke) => (
