ca8941ffc7b559f13b84cea8dec53c541f756a9f
Eylon <eylon.perets@gmail.com>
2022-04-25 16:39:39 +0300

    setup

---
+++ 24_colors_project/src/App.js
@@ -0,0 +1,12 @@
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+function App() {
+  return (
+    <div className="App">
+      <Palette />
+    </div>
+  );
+}
+
+export default App;
